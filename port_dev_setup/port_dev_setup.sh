aec8b44fb912cc5763fa585f13bcc079c289b0a2
EDHsu <bassed1984@gmail.com>
2016-01-26 02:22:17 +0000

    temp

--- port_dev_setup/port_dev_setup.sh
+++ port_dev_setup/port_dev_setup.sh
@@ -1,35 +1,57 @@
 #!/bin/sh
 
 echo "==> 0 - install git, pip"
-sudo apt-get install -y git curl python-dev python-pip
+sudo apt-get install -y git curl python-dev python-pip build-essential
 
-echo "==> 1 - install ultimate vimrc (https://github.com/amix/vimrc)"
+echo "==> 1 - install node"
+curl -sL https://deb.nodesource.com/setup_5.5 | sudo -E bash -
+sudo apt-get install -y npm
+sudo apt-get install -y nodejs
+
+cd ~/.vim/bundle/
+git clone https://github.com/ternjs/tern_for_vim.git
+cd
+
+echo "==> 2 - install pathogen plugin manager (https://github.com/tpope/vim-pathogen)"
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+echo "==> 3 - setup pathogen path manipulation"
+# append row to ~/.vimrc
+echo "execute pathogen#infect()" | sudo tee -a ~/.vimrc
+source ~/.vimrc
+
+echo "==> 4 - install jedi-vim"
+cd ~/.vim/bundle/
+git clone --recursive https://github.com/davidhalter/jedi-vim.git
+cd
+
+echo "==> 5 - install ultimate vimrc (https://github.com/amix/vimrc)"
 #git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 #sh ~/.vim_runtime/install_awesome_vimrc.sh
 
-echo "==> 8 - install vundle"
+echo "==> 6 - install vundle"
 #git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# https://github.com/VundleVim/Vundle.vim#about
-#echo "set nocompatible" | sudo tee -a ~/.vimrc
-#echo "filetype off" | sudo tee -a ~/.vimrc
-#echo "set rtp+=~/.vim/bundle/Vundle.vim" | sudo tee -a ~/.vimrc
-#echo "call vundle#begin()" | sudo tee -a ~/.vimrc
-#echo "Plugin 'VundleVim/Vundle.vim'" | sudo tee -a ~/.vimrc
-#echo "Plugin 'tpope/vim-fugitive'" | sudo tee -a ~/.vimrc
-#echo "Plugin 'L9'" | sudo tee -a ~/.vimrc
-#echo "Plugin 'git://git.wincent.com/command-t.git'" | sudo tee -a ~/.vimrc
-#echo "Plugin 'file:///home/gmarik/path/to/plugin'" | sudo tee -a ~/.vimrc
-#echo "Plugin 'rstacruz/sparkup', {'rtp': 'vim/'}" | sudo tee -a ~/.vimrc
-#echo "Plugin 'user/L9', {'name': 'newL9'}" | sudo tee -a ~/.vimrc
-#echo "call vundle#end()" | sudo tee -a ~/.vimrc
-#echo "filetype plugin indent on" | sudo tee -a ~/.vimrc
+echo "==> 7 - download YouCompleteMe"
+cd ~/.vim/bundle
+git clone https://github.com/Valloric/YouCompleteMe.git
+cd YouCompleteMe/
+git submodule update --init --recursive
+cd
 
+echo "==> 8 - install build-essential cmake"
+sudo apt-get install -y build-essential cmake
+sudo apt-get update
+
+echo "==> 9 - replace .vimrc"
+rm ~/.vimrc
+cp ~/shellscript/shellscript/port_dev_setup/.vimrc ~/
 source ~/.vimrc
 
 
+echo "==> 10 - install c-family language, go language, javascript language and rust"
+#cd ~/.vim/bundle/YouCompleteMe
+#./install.sh --clang-completer --tern-completer
 
 
-echo "==> 9 - install youcompleteme"
-#sudo apt-get install build-essential cmake
-#sudo apt-get install python-dev
